b1796ee8a080924cb8260b23d3610963878e41d8
Bryson Spaulding <brysonian10@hotmail.com>
2018-09-24 09:25:10 -0600

    Finally got the file to work and then changed the gitignore so that it does have a timeline.

---
+++ MySwiftPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("this is pretty neat")
+print("Wow, I can just type stuff in an boom, it appears right below in the console output box")
+print("Dang programming is so easy I don't even know why people are complaining about it")
