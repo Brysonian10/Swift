8ec7c2d4f5dcbc44da358e198c720242c8cb9f89
Bryson Spaulding <brysonian10@hotmail.com>
2018-09-24 09:31:50 -0600

    Just messing around with variables and constants while learning from the swift language guid page.

--- MySwiftPlayground.playground/Contents.swift
+++ MySwiftPlayground.playground/Contents.swift
@@ -6,3 +6,16 @@ var str = "Hello, playground"
 print("this is pretty neat")
 print("Wow, I can just type stuff in an boom, it appears right below in the console output box")
 print("Dang programming is so easy I don't even know why people are complaining about it")
+var myVariable = 7
+myVariable = 14
+let myConstant = 42
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
