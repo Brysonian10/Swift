f2c1f631425385f4c6f7d9782dec1cfb13d4bf34
Bryson Spaulding <brysonian10@hotmail.com>
2018-10-01 08:50:51 -0600

    Deleted everything and tried out something new with structures and classes.

--- MySwiftPlayground.playground/Contents.swift
+++ MySwiftPlayground.playground/Contents.swift
@@ -1,21 +1,55 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+struct SomeStructure {
+    //structure definition goes here
+}
+class SomeClass {
+    // class definition here
+}
 
-var str = "Hello, playground"
-print("this is pretty neat")
-print("Wow, I can just type stuff in an boom, it appears right below in the console output box")
-print("Dang programming is so easy I don't even know why people are complaining about it")
-var myVariable = 7
-myVariable = 14
-let myConstant = 42
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+print("The width of someResolution is \(someResolution.width)")
+// Prints "The width of someResolution is 0"
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+// Prints the width of someVideoMode is 0"
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+// Prints  "The width of someVideoMode is now 1280"
 
-let label = "The width is "
-let width = 94
-let widthLabel = label + String(width)
+let hd =  Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+
+print("cinema is now \(cinema.width) pixels wide")
+// Prints cinema is now 2048 pixels wide:
+
+print ("hd is still \(hd.width) pixels wide")
+// Prints "hd is still 1920 pixels wide"
+
+enum CompassPoint {
+    case north, south, east, west
+    mutating func turnNorth() {
+    self = .north
+    }
+}
+var currentDirection = CompassPoint.west
+let rememberedDirection = currentDirection
+currentDirection.turnNorth()
+
+print("The current direction is \(currentDirection)")
+print("The remembered direction is \(rememberedDirection)")
+// Prints "The current direction is north"
+// Prints "The remembered direcion is west"
 
 
-let apples = 3
-let oranges = 5
-let appleSummary = "I have \(apples) apples."
-let fruitSummary = "I have \(apples + oranges) pieces of fruit."
